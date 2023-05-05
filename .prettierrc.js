87cb99d5bcee5a63d1ccd0376c5b8c3f7d56f3f0
Adrian Kuraszkiewicz <goieh@WPLT002513.bayer.cnb>
2023-05-05 17:55:01 +0200

    initial app

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  tabs: false,
+  semi: false,
+  singleQuote: true,
+  tailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+};
